55f514c2201493d4e1ccaf12a1e8885ddaafbf8e
shravan-kumar7 <121107760+shravan-kumar7@users.noreply.github.com>
2024-03-12 13:30:39 +0530

    6b_floyd.cpp

--- 6b_floyd.cpp
+++ 6b_floyd.cpp
@@ -16,29 +16,15 @@ void floyds(int p[10][10], int n) {
 }
 
 int main() {
-    int p[10][10], w, n, e, u, v, i, j;
+    int p[10][10], n, i, j;
     
     cout << "\nEnter the number of vertices: ";
     cin >> n;
-    cout << "\nEnter the number of edges:\n";
-    cin >> e;
 
+    cout << "\nEnter the weight matrix (999 if no edge)\n";
     for (i = 1; i <= n; i++) {
         for (j = 1; j <= n; j++)
-            p[i][j] = (i == j) ? 0 : INT_MAX;
-    }
-
-    for (i = 1; i <= e; i++) {
-        cout << "\nEnter the end vertices of edge " << i << " with its weight:\n";
-        cin >> u >> v >> w;
-        p[u][v] = w;
-    }
-
-    cout << "\nMatrix of input data:\n";
-    for (i = 1; i <= n; i++) {
-        for (j = 1; j <= n; j++)
-            cout << p[i][j] << "\t";
-        cout << "\n";
+            cin>>  p[i][j];
     }
     clock_t start = clock();
   
@@ -54,16 +40,9 @@ int main() {
         cout << "\n";
     }
 
-    cout << "\nThe shortest paths are:\n";
-    for (i = 1; i <= n; i++)
-        for (j = 1; j <= n; j++)
-            if (i != j)
-                cout << "\n<" << i << "," << j << ">=" << p[i][j];
-
     double execution_time = double(end - start) / double(CLOCKS_PER_SEC);
 
     // Display execution time
-    cout<<"";
 	cout << "Time taken by function: " << fixed<< execution_time << " seconds" << endl;
     return 0;
 }
